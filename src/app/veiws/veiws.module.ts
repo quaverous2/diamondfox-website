ed7e1b6365adb5e3cafc2c45cb704c7ccafdfb1a
quaverous2 <contact@antonioancona.it>
2022-06-26 01:46:28 +0200

    Implemented font family hirearchy, styling and basic rotuing structure

--- src/app/veiws/veiws.module.ts
+++ src/app/veiws/veiws.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ]
 })
 export class VeiwsModule { }
